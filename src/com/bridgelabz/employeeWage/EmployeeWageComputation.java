ccb4e6599259e5c1b77930d9d19f0c5286fa102e
chutki12 <priyankachaudhary2068>
2022-07-05 00:20:43 +0530

    ADDED:Welcome To Employee Wage

---
+++ src/com/bridgelabz/employeeWage/EmployeeWageComputation.java
@@ -0,0 +1,10 @@
+package com.bridgelabz.employeeWage;
+
+public class EmployeeWageComputation {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome TO Employee Wage Computation");
+
+	}
+
+}
