dba7cdf71f97b6c346b4aa8bdb8a6cfa76d1ab48
chutki12 <priyankachaudhary2068>
2022-07-05 00:48:08 +0530

    REFACTORED: Employee Daily Wage

--- src/com/bridgelabz/employeeWage/EmployeeWageComputation.java
+++ src/com/bridgelabz/employeeWage/EmployeeWageComputation.java
@@ -6,9 +6,15 @@ public class EmployeeWageComputation {
 		System.out.println("Welcome TO Employee Wage Computation");
 		
 		int is_present= 1;
+		int Emp_wage_per_hr = 20;
+		int working_hr = 8;
+		
 		double emp_attendance = Math.floor(Math.random ()* 10) % 2;
+		
+		int daily_wage = working_hr * Emp_wage_per_hr ;
+		
 		if ( emp_attendance == is_present )
-		System.out.println("Employee is Present");
+			System.out.println("Employee is Present & working hr = "+ working_hr + " hr " + "  daily wage = " + daily_wage);
 		else
 			System.out.println("Employee is Absent");
 
