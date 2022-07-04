5ba636a3b89d4c78149659412d8d53f00c9f99f1
chutki12 <priyankachaudhary2068>
2022-07-05 00:34:44 +0530

    REFACTORED:Employee Attendance

--- src/com/bridgelabz/employeeWage/EmployeeWageComputation.java
+++ src/com/bridgelabz/employeeWage/EmployeeWageComputation.java
@@ -4,6 +4,13 @@ public class EmployeeWageComputation {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome TO Employee Wage Computation");
+		
+		int is_present= 1;
+		double emp_attendance = Math.floor(Math.random ()* 10) % 2;
+		if ( emp_attendance == is_present )
+		System.out.println("Employee is Present");
+		else
+			System.out.println("Employee is Absent");
 
 	}
 
